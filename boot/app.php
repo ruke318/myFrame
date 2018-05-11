79bacc9471fdd1aec4b71889034b95b3391f07a9
signer <signer@cnmtoc.com>
2018-05-11 13:35:03 +0800

    重写注释

--- boot/app.php
+++ boot/app.php
@@ -5,26 +5,26 @@ header("charset=utf-8");
 ini_set('always_populate_raw_post_data', '-1');
 date_default_timezone_set('Asia/Shanghai');
 
-// autoload
+// composer 启动文件
 include ROOT.'/vendor/autoload.php';
 
-//debug register
+// 自己的调试
 DeBug::register();
 
-//add service to di
+// 想容器中注册服务
 Di\Di::register([
     'file' => \Di\Cache\File::class,
     'redis' => \Di\Cache\Redis::class,
     'cache' => \Di\Cache::class
 ]);
 
-// Eloquent ORM
+// tp的orm
 Db::setConfig(\Lib\Config::get('database'));
 
-//引入路由文件
+// 引入路由文件
 Route::group(['namespace' => 'App\Controllers'], function () {
     include ROOT.'/route/web.php';
 });
 
-//分发路由
+// 分发路由
 Route::dispatch();
